ac7858d62bd9c34b1389c75340cef343ea78df10
Ankit kumar <ankitkur@iitk.ac.in>
2015-12-02 01:46:23 +0530

    includes all headers and trackbars

--- headers.h
+++ headers.h
@@ -3,6 +3,16 @@
 #include <iostream>
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include "trackbars.h"
+#include <math.h>
 using namespace cv;
 using namespace std;
+
+void trackbars(int *h1,int *h2,int *s1,int *s2,int *v1,int *v2){
+	namedWindow("trackWindow",0);
+	createTrackbar("H_MIN","trackWindow",h1,255);
+	createTrackbar("H_MAX","trackWindow",h2,255);
+	createTrackbar("S_MIN","trackWindow",s1,255);
+	createTrackbar("S_MAX","trackWindow",s2,255);
+	createTrackbar("V_MIN","trackWindow",v1,255);
+	createTrackbar("V_MAX","trackWindow",v2,255);
+}
