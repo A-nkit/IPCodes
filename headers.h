170f2bb8d09a5bc4a0ea1cc67e366fb8878ac6cd
Ankit kumar <ankitkur@iitk.ac.in>
2015-12-01 09:34:09 +0530

    all headers included with namespace

---
+++ headers.h
@@ -0,0 +1,8 @@
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <iostream>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "trackbars.h"
+using namespace cv;
+using namespace std;
